48fa202286b656804021301b68f43ed77467c5bb
Peter Radko <cmtvpetya@gmail.com>
2025-04-24 17:28:58 +0300

    Disable YAML stringify automatic newline insertion

--- packages/transpiler/src/utils/str.ts
+++ packages/transpiler/src/utils/str.ts
@@ -23,7 +23,7 @@ export function parseYAML(text: string): any {
 }
 
 export function stringifyYAML(content: any): string {
-    let text = YAML.stringify(content, { indent: 4 });
+    let text = YAML.stringify(content, { indent: 4, lineWidth: 0 });
     // Convert all ways YAML stringify can output multiline strings to just "...: |\n" notation
     text = text.replace(/^(\s*.+): [>|][+-]?\n/gm, '$1: |\n');
     text = text.trim();
